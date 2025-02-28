431be4b13160ab492cd4becfd34e8c01babf7c12
Casey Schmitz <casey.schmitz@protonmail.com>
2025-02-28 14:39:50 -0600

    modularized

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
   // Copy the assets folder to the output
   eleventyConfig.addPassthroughCopy("assets");
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+};
 
-  return {
-    dir: {
-      input: ".",
-      includes: "_includes",
-      output: "_site",
-    },
-  };
+export const config = {
+  pathPrefix: "/shaker-door-calculator/"
 }
