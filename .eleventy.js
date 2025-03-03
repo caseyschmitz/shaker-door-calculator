504773e9e9bf78fddf9d3e6f802538715e91ce5b
Casey Schmitz <casey.schmitz@protonmail.com>
2025-03-03 10:08:21 -0600

    layout-ified. very much in-progress

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,36 @@ import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
   // Copy the assets folder to the output
-  eleventyConfig.addPassthroughCopy("assets");
+  /*
+  eleventyConfig.addPassthroughCopy({
+    "./assets/": "/"
+  });
+  */
+ eleventyConfig.addPassthroughCopy("./assets/");
+
+  eleventyConfig.addBundle("css");
+  eleventyConfig.addBundle("js");
+
+  /*
+  // Adds the {% css %} paired shortcode
+	eleventyConfig.addBundle("css", {
+		toFileDirectory: "dist",
+	});
+	// Adds the {% js %} paired shortcode
+	eleventyConfig.addBundle("js", {
+		toFileDirectory: "dist",
+	});
+  */
+
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 };
 
 export const config = {
-  pathPrefix: "/shaker-door-calculator/"
+  pathPrefix: "/shaker-door-calculator/",
+  dir: {
+    input: "content",
+    includes: "../_includes",
+    data: "../_data",
+    output: "_site"
+  }
 }
---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Shaker Door Calculator",
+	url: "https://caseyschmitz.com/shaker-door-calculator/",
+	language: "en",
+	description: "A simple calculator that generates a cutlist and procedures for building shaker style cabinet doors.",
+	author: {
+		name: "Casey Schmitz",
+		email: "casey.schmitz@protonmail.com",
+		url: "https://caseyschmitz.com/"
+	}
+}
